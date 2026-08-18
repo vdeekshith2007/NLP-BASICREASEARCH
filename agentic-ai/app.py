031bc94b85cdf95bbb5155b269f1d9bb4c9bfc78
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-08-18 17:13:59 +0530

    manage

--- agentic-ai/app.py
+++ agentic-ai/app.py
@@ -1,5 +1,5 @@
 
-# import langchain as the frame work
+# import langchain as the frame work like streamlit, langchain,langgraph
 
 from src.langgraphagenticai.main import load_langgraph_agenticai_app
 
